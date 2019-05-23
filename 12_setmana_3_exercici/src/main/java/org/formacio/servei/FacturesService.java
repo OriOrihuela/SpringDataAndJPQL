8999e4235c16cc43a1a7498d6bda0856493eebf3
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-05-23 19:10:11 +0200

    test_es_crud_repositori passed :)

--- 12_setmana_3_exercici/src/main/java/org/formacio/servei/FacturesService.java
+++ 12_setmana_3_exercici/src/main/java/org/formacio/servei/FacturesService.java
@@ -1,20 +1,31 @@
 package org.formacio.servei;
 
 import org.formacio.domain.Factura;
+import org.formacio.repositori.FacturesRepositori;
+import org.springframework.stereotype.Service;
 
+@Service
 public class FacturesService {
 
-	
-	/*
-	 * Aquest metode ha de carregar la factura amb id idFactura i afegir una nova linia amb les dades
-	 * passades (producte i totalProducte)
-	 * 
-	 * S'ha de retornar la factura modificada
-	 * 
-	 * Per implementar aquest metode necessitareu una referencia (dependencia) a FacturesRepositori
-	 */
-	public Factura afegirProducte (long idFactura, String producte, int totalProducte) {
-		
-		return null;
-	}
+    /* ---- Properties of the class ---- */
+    private FacturesRepositori facturesRepositori = null;
+
+
+    /* ---- Getters ---- */
+    public FacturesRepositori getFacturesRepositori() {
+        return facturesRepositori;
+    }
+
+    /*
+     * Aquest metode ha de carregar la factura amb id idFactura i afegir una nova linia amb les dades
+     * passades (producte i totalProducte)
+     *
+     * S'ha de retornar la factura modificada
+     *
+     * Per implementar aquest metode necessitareu una referencia (dependencia) a FacturesRepositori
+     */
+    public Factura afegirProducte(long idFactura, String producte, int totalProducte) {
+
+        return null;
+    }
 }
